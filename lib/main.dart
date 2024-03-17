a107655ac36f854ae247221519e7f9cc7b5a2548
raaffiy <raafimaulana43@gmail.com>
2024-03-17 10:33:46 +0700

    update type

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:admin/pages/intro_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:get/get.dart'; // Tambahkan import GetX di sini
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,13 +13,16 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+      home: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroPage(),
+      ),
     );
   }
 }
